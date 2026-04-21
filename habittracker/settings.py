c98eea1c3e4578d992d0e07e49a314bcbe46ad42
piyush-tech1 <queriespiyush1234@gmail.com>
2026-04-21 23:25:58 +0530

    saving changes before pull

--- habittracker/settings.py
+++ habittracker/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +115,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/habits/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
